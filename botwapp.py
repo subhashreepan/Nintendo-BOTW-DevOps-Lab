5bfdcc1f446bc09b9490fafc10a8b72fc1eb16e5
subhashreepan <L00187418@atu.ie>
2024-11-16 12:16:47 +0000

    Committing changes to merge shrine-history-seed-ui branch with korak-seed-ui branch

--- botwapp.py
+++ botwapp.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     print ("Shrine-history-ui")
-    return "Nintendo-botw-ui Shrine history ui"
+    return "Nintendo-botw-ui korak seed ui"
 
 if __name__ == "__main__":
     app.run(debug=True)
